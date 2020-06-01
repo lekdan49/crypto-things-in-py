6435b5ecbbc9ff76f1995962a726e9ceaf3e4848
lekdan49 <14988063+lekdan49@users.noreply.github.com>
2020-06-01 23:52:09 +1000

    add caesar cipher

--- vigenere.py
+++ vigenere.py
@@ -23,16 +23,9 @@ def vigenere(plaintext, key):
 
         position_in_list = alphabet.index(letter)
         new_letter = position_in_list + shift_values[0 + index_pos]
-        new_letter = new_letter
 
-        if new_letter > 26:
-            extra = position_in_list + shift_values[0 + index_pos]
-            extra = extra - 26
-            ciphertext.append(alphabet[extra])
-
-
-        else:
-            ciphertext.append(alphabet[new_letter])
+        vigenered = alphabet[new_letter % len(alphabet)]
+        ciphertext.append(vigenered)
 
         index_pos += 1
     return ciphertext
